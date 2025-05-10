2e9792132face38dcf317a99624178e546187860
Huy Nguyen <little34306xd@gmail.com>
2025-05-10 22:10:57 +0700

    1.2

--- mdc0/ContentView.swift
+++ mdc0/ContentView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 import Drops
+import notify
+
+func toggleLiquidDetectionIcon(isEnabled: Bool) {
+    if isEnabled {
+        UIStatusBarServer.addStatusBarItem(40)
+    } else {
+        UIStatusBarServer.removeStatusBarItem(40)
+    }
+}
+func applyStatusBarStyleOverride(value: Int32) {
+    UIStatusBarServer.addStyleOverrides(value)
+}
+func removeStatusBarStyleOverride(value: Int32) {
+    UIStatusBarServer.removeStyleOverrides(value)
+}
 
 struct ContentView: View {
     @State private var darkDockStatus: String = ""
@@ -15,6 +30,14 @@ struct ContentView: View {
     @State private var homeBarStatus: String = ""
     @State private var lockscreenIconsStatus: String = ""
     @State private var appSwitcherBlurStatus: String = ""
+    @State private var cameraShutterSoundStatus: String = ""
+    @State private var passcodeThemeStatus: String = ""
+    @State private var selectedTelephonyUIVersion: String = "TelephonyUI-9"
+    @State private var liquidDetectionStatus: String = ""
+    @State private var statusBarOverrideStatus: String = ""
+    @State private var customOverrideValue: String = ""
+    @State private var selectedOverrideValue: Int32 = 2147483647
+    @State private var showingOverrideOptions: Bool = false
     @State private var respringStatus: String = ""
     
     var body: some View {
@@ -23,13 +46,11 @@ struct ContentView: View {
                 Section("Available Tweaks") {
                     VStack(alignment: .leading) {
                         Button("Hide Dock", systemImage: "dock.rectangle") {
-                            // Process dark dock material
                             let darkPath = strdup("/System/Library/PrivateFrameworks/CoreMaterial.framework/dockDark.materialrecipe")
                             let darkResult = poc(darkPath)
                             darkDockStatus = darkResult == 0 ? "Success" : "Failed"
                             free(darkPath)
                             
-                            // Process light dock material
                             let lightPath = strdup("/System/Library/PrivateFrameworks/CoreMaterial.framework/dockLight.materialrecipe")
                             let lightResult = poc(lightPath)
                             lightDockStatus = lightResult == 0 ? "Success" : "Failed"
@@ -164,7 +185,6 @@ struct ContentView: View {
                     
                     VStack(alignment: .leading) {
                         Button("Remove App Switcher Blur", systemImage: "square.stack.fill") {
-                            // Process app switcher blur files
                             let paths = [
                                 "/System/Library/PrivateFrameworks/SpringBoard.framework/homeScreenBackdrop-application.materialrecipe",
                                 "/System/Library/PrivateFrameworks/SpringBoard.framework/homeScreenBackdrop-switcher.materialrecipe"
@@ -199,6 +219,229 @@ struct ContentView: View {
                     }
                     .foregroundColor(.white)
                     .listRowBackground(Color.indigo)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Killing the Camera app may turn back on the sound")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 5)
+                            
+                        Button("Disable Camera Shutter Sound", systemImage: "camera.fill") {
+                            let paths = [
+                                "/System/Library/Audio/UISounds/photoShutter.caf",
+                                "/System/Library/Audio/UISounds/begin_record.caf",
+                                "/System/Library/Audio/UISounds/end_record.caf",
+                                "/System/Library/Audio/UISounds/Modern/camera_shutter_burst.caf",
+                                "/System/Library/Audio/UISounds/Modern/camera_shutter_burst_begin.caf",
+                                "/System/Library/Audio/UISounds/Modern/camera_shutter_burst_end.caf"
+                            ]
+                            
+                            var successCount = 0
+                            for filePath in paths {
+                                let path = strdup(filePath)
+                                let result = poc(path)
+                                if result == 0 {
+                                    successCount += 1
+                                }
+                                free(path)
+                            }
+                            
+                            cameraShutterSoundStatus = "Success: \(successCount)/\(paths.count) files processed"
+                            Drops.show(Drop(title: "Disable Camera Shutter Sound", subtitle: cameraShutterSoundStatus, icon: UIImage(systemName: "camera.fill")))
+                        }
+                        .symbolRenderingMode(.hierarchical)
+                        HStack {
+                                Text("Camera Shutter Sound Status")
+                                .foregroundStyle(.white)
+                                Spacer()
+                            Circle()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(cameraShutterSoundStatus.isEmpty ? Color(uiColor: .lightGray) : cameraShutterSoundStatus.contains("Success") ? Color.green : Color.red)
+                                .background {
+                                    Circle()
+                                        .stroke(.white, lineWidth: 3)
+                                }
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .listRowBackground(Color.red)
+                    
+                    VStack(alignment: .leading) {
+                        Picker("Select TelephonyUI", selection: $selectedTelephonyUIVersion) {
+                            Text("TelephonyUI-7").tag("TelephonyUI-7")
+                            Text("TelephonyUI-8").tag("TelephonyUI-8")
+                            Text("TelephonyUI-9").tag("TelephonyUI-9")
+                            Text("TelephonyUI-10").tag("TelephonyUI-10")
+                            //Text("All Versions").tag("All")
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                        .foregroundColor(.white)
+                        .accentColor(.white)
+                        
+                        Button("Remove Passcode Theme (may not work)", systemImage: "lock.fill") {
+                            let buttonSuffixes = [
+                                "0---white.png",
+                                "1---white.png",
+                                "2-A B C--white.png",
+                                "3-D E F--white.png",
+                                "4-G H I--white.png",
+                                "5-J K L--white.png",
+                                "6-M N O--white.png",
+                                "7-P Q R S--white.png",
+                                "8-T U V--white.png",
+                                "9-W X Y Z--white.png"
+                            ]
+                            
+                            var allPaths: [String] = []
+                            
+                            if selectedTelephonyUIVersion == "All" {
+                                let versions = ["TelephonyUI-7", "TelephonyUI-8", "TelephonyUI-9", "TelephonyUI-10"]
+                                for version in versions {
+                                    for suffix in buttonSuffixes {
+                                        allPaths.append("/var/mobile/Library/Caches/\(version)/en-\(suffix)")
+                                    }
+                                }
+                            } else {
+                                for suffix in buttonSuffixes {
+                                    allPaths.append("/var/mobile/Library/Caches/\(selectedTelephonyUIVersion)/en-\(suffix)")
+                                }
+                            }
+                            
+                            var successCount = 0
+                            for filePath in allPaths {
+                                let path = strdup(filePath)
+                                let result = poc(path)
+                                if result == 0 {
+                                    successCount += 1
+                                }
+                                free(path)
+                            }
+                            
+                            passcodeThemeStatus = "Success: \(successCount)/\(allPaths.count) files processed"
+                            Drops.show(Drop(title: "Remove Passcode Theme", subtitle: passcodeThemeStatus, icon: UIImage(systemName: "lock.fill")))
+                        }
+                        .symbolRenderingMode(.hierarchical)
+                        HStack {
+                                Text("Passcode Theme Status")
+                                .foregroundStyle(.white)
+                                Spacer()
+                            Circle()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(passcodeThemeStatus.isEmpty ? Color(uiColor: .lightGray) : passcodeThemeStatus.contains("Success") ? Color.green : Color.red)
+                                .background {
+                                    Circle()
+                                        .stroke(.white, lineWidth: 3)
+                                }
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .listRowBackground(Color.pink)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Works on iOS 16.0 - 18.2, kill this app make it disappear")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 5)
+                        
+                        Button(liquidDetectionStatus == "Enabled" ? "Disable Liquid Detection Icon" : "Enable Liquid Detection Icon", systemImage: liquidDetectionStatus == "Enabled" ? "drop" : "drop.fill") {
+                            let newStatus = liquidDetectionStatus != "Enabled" ? "Enabled" : "Disabled"
+                            liquidDetectionStatus = newStatus
+                            
+                            toggleLiquidDetectionIcon(isEnabled: newStatus == "Enabled")
+                            
+                            Drops.show(Drop(
+                                title: "Liquid Detection", 
+                                subtitle: newStatus == "Enabled" ? "Icon enabled" : "Icon disabled", 
+                                icon: UIImage(systemName: newStatus == "Enabled" ? "drop.fill" : "drop")
+                            ))
+                        }
+                        .symbolRenderingMode(.hierarchical)
+                        
+                        HStack {
+                                Text("Liquid Detection Status")
+                                .foregroundStyle(.white)
+                                Spacer()
+                            Circle()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(liquidDetectionStatus.isEmpty ? Color(uiColor: .lightGray) : liquidDetectionStatus == "Enabled" ? Color.green : Color.red)
+                                .background {
+                                    Circle()
+                                        .stroke(.white, lineWidth: 3)
+                                }
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .listRowBackground(Color.blue)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Works on iOS 16.0 - 18.2, kill this app to remove")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 5)
+                        
+                        HStack {
+                            Picker("Status Bar Override", selection: $selectedOverrideValue) {
+                                Text("UNKNOWN GOD").tag(Int32(2147483647)) // INT_MAX
+                                Text("Calling").tag(Int32(1))
+                                Text("Microphone").tag(Int32(4))
+                                Text("FaceTime").tag(Int32(16))
+                                Text("Hotspot").tag(Int32(8))
+                                Text("AirPlay").tag(Int32(32))
+                                Text("Location").tag(Int32(64))
+                                Text("Connect Cable").tag(Int32(512))
+                                Text("Custom").tag(Int32(-1))
+                            }
+                            .pickerStyle(MenuPickerStyle())
+                            .foregroundColor(.white)
+                            .accentColor(.white)
+                            .frame(maxWidth: .infinity)
+                            
+                            if selectedOverrideValue == -1 {
+                                TextField("Value", text: $customOverrideValue)
+                                    .keyboardType(.numberPad)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .frame(width: 80)
+                            }
+                        }
+                        
+                        HStack {
+                            Button(statusBarOverrideStatus == "Enabled" ? "Disable Status Bar Override" : "Enable Status Bar Override", systemImage: statusBarOverrideStatus == "Enabled" ? "xmark.circle" : "checkmark.circle") {
+                                let newStatus = statusBarOverrideStatus != "Enabled" ? "Enabled" : "Disabled"
+                                statusBarOverrideStatus = newStatus
+                                
+                                let valueToUse = selectedOverrideValue == -1 ? (Int32(customOverrideValue) ?? 0) : selectedOverrideValue
+                                
+                                if newStatus == "Enabled" {
+                                    applyStatusBarStyleOverride(value: valueToUse)
+                                } else {
+                                    removeStatusBarStyleOverride(value: valueToUse)
+                                }
+                                
+                                Drops.show(Drop(
+                                    title: "Status Bar Override", 
+                                    subtitle: "\(newStatus) with value: \(valueToUse)", 
+                                    icon: UIImage(systemName: newStatus == "Enabled" ? "checkmark.circle" : "xmark.circle")
+                                ))
+                            }
+                            .symbolRenderingMode(.hierarchical)
+                            .frame(maxWidth: .infinity)
+                        }
+                        
+                        HStack {
+                            Text("Status Bar Override Status")
+                                .foregroundStyle(.white)
+                            Spacer()
+                            Circle()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(statusBarOverrideStatus.isEmpty ? Color(uiColor: .lightGray) : statusBarOverrideStatus == "Enabled" ? Color.green : Color.red)
+                                .background {
+                                    Circle()
+                                        .stroke(.white, lineWidth: 3)
+                                }
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .listRowBackground(Color.green)
                 }
                 Section("Status") {
                     HStack {
@@ -242,7 +485,7 @@ struct ContentView: View {
 
                 Section {
                     // Credit
-                    Text("Made by [34306](https://github.com/34306), UI by [timi2506](https://github.com/timi2506), using CVE-2025-24203 by Ian Beer and my old mdc/kfd stuff\nSupport iOS 15.0 - 18.3.2, you need to respring by hand to take effect using Library trick or using [this app](https://github.com/34306/mdc0/releases/download/1.0/respringapp.ipa) thanks to Thea")
+                    Text("Made by [34306](https://github.com/34306), UI by [timi2506](https://github.com/timi2506), using CVE-2025-24203 by Ian Beer and my old mdc/kfd stuff\nSupport iOS 15.0 - 18.3.2, you need to respring to take effect using [this app](https://github.com/34306/mdc0/releases/download/1.0/respringapp.ipa) thanks to Thea\nThanks to [DuyTran](https://x.com/TranKha50277352) for the Liquid status bar and fake status")
                         .font(.caption)
                         .foregroundColor(.gray)
                 }
--- mdc0/mdc0-Bridging-Header.h
+++ mdc0/mdc0-Bridging-Header.h
@@ -2,5 +2,16 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 int poc(char *path);
 int backboard_respring(void);
+
+// UIStatusBarServer private API declaration
+@interface UIStatusBarServer : NSObject
++ (void)addStatusBarItem:(int32_t)arg1;
++ (void)removeStatusBarItem:(int32_t)arg1;
++ (void)addStyleOverrides:(int32_t)arg1;
++ (void)removeStyleOverrides:(int32_t)arg1;
+@end
